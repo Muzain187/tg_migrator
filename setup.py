316085007397ec6631ae47c03d7922ca27a8037c
Muzain187 <muzain.ashraf@gmail.com>
2024-08-10 01:29:16 +0530

    created package 0.1.5

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='tg_migrator',
-    version='0.1.4',
+    version='0.1.5',
     author='Mohammad Ashraf',
     description='migration tool for tigergraph',
     packages=find_packages(),
     install_requires=[
         'configparser',
         'pyTigerGraph',
+        'tqdm'
     ],
     entry_points={
         'console_scripts': [
